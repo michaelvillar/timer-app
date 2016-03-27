0e01e34f971a21935b6f62505e5293b620d2926c
Michael Villar <me@michaelvillar.com>
2016-03-27 00:38:15 -0700

    Initial Commit

---
+++ Timer/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Timer
+//
+//  Created by Michael Villar on 3/27/16.
+//  Copyright © 2016 Michael Villar. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
