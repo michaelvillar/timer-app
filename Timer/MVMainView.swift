9cc996c3aa21778a9a8a4b5706aa401e3831d48c
Ali Karbassi <ali@karbassi.com>
2020-04-12 01:46:59 -0500

    Update text for option "Show in Dock"
    It wasn't clear.
    
    Fixes #73

--- Timer/MVMainView.swift
+++ Timer/MVMainView.swift
@@ -26,7 +26,7 @@ class MVMainView: NSView {
     super.init(frame: frameRect)
 
     self.contextMenu = NSMenu(title: "Menu")
-    menuItem = NSMenuItem(title:"Show in Dock", action:#selector(self.toggleShowInDock), keyEquivalent:"")
+    menuItem = NSMenuItem(title:"Show timer badge in dock", action:#selector(self.toggleShowInDock), keyEquivalent:"")
     self.contextMenu?.addItem(menuItem!)
 
     let nc = NotificationCenter.default
@@ -53,32 +53,31 @@ class MVMainView: NSView {
 
   override func draw(_ dirtyRect: NSRect) {
     super.draw(dirtyRect)
-    
+
     let windowHasFocus = self.window?.isKeyWindow ?? false
-    
-    
+
     var topColor = NSColor(srgbRed: 242/255, green: 241/255, blue: 242/255, alpha: 1.000)
     var bottomColor = NSColor(srgbRed: 214/255, green: 212/255, blue: 214/255, alpha: 1.000)
-    
+
     if !windowHasFocus {
       topColor = NSColor(srgbRed: 246/255, green: 246/255, blue: 246/255, alpha: 1.000)
       bottomColor = topColor
     }
-    
+
     if isDarkMode {
         topColor = NSColor(srgbRed: 39/255, green: 39/255, blue: 39/255, alpha: 1.000)
         bottomColor = NSColor(srgbRed: 18/255, green: 18/255, blue: 18/255, alpha: 1.000)
     }
-    
+
     if #available(OSX 10.13, *) {
         topColor = NSColor(named: "background-top-color")!
         bottomColor = NSColor(named: "background-bottom-color")!
     }
-    
+
     let gradient = NSGradient(colors: [topColor, bottomColor])
     let radius: CGFloat = 4.53
     let path = NSBezierPath(roundedRect: self.bounds, xRadius: radius, yRadius: radius)
-    
+
     gradient?.draw(in: path, angle: -90)
   }
 
